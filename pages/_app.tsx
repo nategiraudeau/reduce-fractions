1b54bee4a97bcff786e99a19c68802cff108583d
nategiraudeau <natefg305@gmail.com>
2021-09-01 17:09:04 -0400

    redesign

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,15 +2,14 @@ import React from 'react';
 import type { AppProps } from 'next/app';
 import Head from 'next/head';
 
-import '../css/index.css';
-import '../css/form.css';
+import '../scss/index.scss';
 
 export default function _App({ Component, pageProps }: AppProps) {
     return (
         <div>
             <Head>
                 <title>Reduce Fractions</title>
-                <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet" />
+                <link href="https://fonts.googleapis.com/css2?family=Rubik:wght@100;200;300;400;500;600;700;800;900&family=Rubik+Mono+One&family=Roboto+Mono:wght@400;500;600;700;800&family=Fira+Mono&display=swap" rel="stylesheet" />
             </Head>
             <Component {...pageProps} />
         </div>
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,15 @@
+import { faGithub } from '@fortawesome/free-brands-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React, { useState } from 'react';
+import * as Icons from 'react-feather';
 
 import reduceFraction from '../reduceFraction';
 
 const _: React.FC = () => {
     const [numerator, setNumerator] = useState<number | undefined>(4);
     const [denominator, setDenominator] = useState<number | undefined>(8);
+    const [newFraction, setNewFraction] = useState<[number, number]>([1, 1]);
+    const [animate, setAnimate] = useState(false);
 
     function handleChangeNumerator(v: string) {
         const value = Number.parseInt(v);
@@ -27,19 +32,46 @@ const _: React.FC = () => {
     }
 
     return (
-        <div className="App">
-            <div className="background-triangle"></div>
+        <div className={`_app${animate ? ' animate' : ''}`}>
+            <div className="circle-green"></div>
+            <div className="circle-yellow"></div>
+            <div className="header">
+                <h1>Reduce <br /> Fractions </h1>
+                <div>
+                    <a href="https://github.com/nategiraudeau/reduce-fractions" rel="noopener noreferrer" target="_blank" className="github">
+                        <Icons.ArrowRight opacity={0.3} size={18} />
+                        <FontAwesomeIcon width={20} icon={faGithub} />
+                        <h4>nategiraudeau/reduce-fractions</h4>
+                    </a>
+                </div>
+            </div>
             <div className="form">
-                <h1>Reduce <br /> Fractions</h1>
-                <div className="inputs">
-                    <input value={numerator} onChange={(e) => handleChangeNumerator(e.target.value)} />
-                    <hr style={{ opacity: '0', }} className="fraction-divider"></hr>
-                    <input value={denominator} onChange={(e) => handleChangeDenominator(e.target.value)} />
-                    <button onClick={() => {
+                <div className="inputs-container">
+                    <div className="inputs">
+                        <input value={numerator} onChange={(e) => handleChangeNumerator(e.target.value)} />
+                        <input value={denominator} onChange={(e) => handleChangeDenominator(e.target.value)} />
+                    </div>
+                    <div className="inputs-new">
+                        <input value={newFraction[0]} />
+                        <input value={newFraction[1]} />
+                    </div>
+                </div>
+                <button onClick={async () => {
+                    if (!animate) {
                         const fraction = reduceFraction(numerator, denominator);
+                        if (numerator === fraction[0] && denominator === fraction[1]) {
+                            return;
+                        }
+                        setNewFraction(fraction);
+                        setAnimate(true);
+                        await new Promise((resolve, reject) => {
+                            setTimeout(resolve, 600);
+                        });
                         setNumerator(fraction[0]);
                         setDenominator(fraction[1]);
-                    }}>Reduce</button></div>
+                        setAnimate(false);
+                    }
+                }}><Icons.ArrowRight size={18} visibility="hidden" /> <span className="label">Reduce</span> <span className="icon"><Icons.ArrowRight strokeWidth={2.5} size={18} /></span></button>
             </div>
         </div>
     );
